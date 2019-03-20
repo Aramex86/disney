084c65cecdb9d840b8219a1671066befedc4f044
Aramex <slavbez@mail.ru>
2019-03-20 19:15:28 +0200

    moana

--- js/script.js
+++ js/script.js
@@ -2,14 +2,48 @@ $(document).ready(function () {
     $('.slider').bxSlider({
         pager:false,
         minSlides:3,
-        moveSlides:3,
-        slideWidth:418,
-        keyboardEnabled:true
-
-
-
-
+        moveSlides:1,
+        slideWidth:362,
+        keyboardEnabled:true,
+        //randomStart:true
     });
+    $('.slide1__card--info-link').on('click',function () {
+        $('.big__cardwrap--1').fadeIn(500).toggleClass("bla");
+
+        $('.section__sliderwrap').fadeOut();
+        $('.big__cardwrap--1-btn').click(function () {
+            $('.big__cardwrap--1').fadeOut().removeClass("bla");
+            $('.section__sliderwrap').fadeIn();
+        });
+    });
+    $('.slide2__card--info-link').on('click',function () {
+        $('.big__cardwrap--2').fadeIn(500).toggleClass("bla");
+
+        $('.section__sliderwrap').fadeOut();
+        $('.big__cardwrap--2-btn').click(function () {
+            $('.big__cardwrap--2').fadeOut().removeClass("bla");
+            $('.section__sliderwrap').fadeIn();
+        });
+    });
+    $('.slide3__card--info-link').on('click',function () {
+        $('.big__cardwrap--3').fadeIn(500).toggleClass("bla");
+
+        $('.section__sliderwrap').fadeOut();
+        $('.big__cardwrap--3-btn').click(function () {
+            $('.big__cardwrap--3').fadeOut().removeClass("bla");
+            $('.section__sliderwrap').fadeIn();
+        });
+    });
+    $('.slide4__card--info-link').on('click',function () {
+        $('.big__cardwrap--4').fadeIn(500).toggleClass("bla");
+
+        $('.section__sliderwrap').fadeOut();
+        $('.big__cardwrap--4-btn').click(function () {
+            $('.big__cardwrap--4').fadeOut().removeClass("bla");
+            $('.section__sliderwrap').fadeIn();
+        });
+    });
+
 
 });
 
